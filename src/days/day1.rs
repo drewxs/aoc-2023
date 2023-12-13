2a3767e2e41d83f3d42be271f5d13b7591ee92a0
Andrew X. Shah <drewshah0@gmail.com>
2023-12-13 09:49:36 -0700

    refactor/optimize day 1 part 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,16 +1,15 @@
 use crate::aoc::AOC;
 
 
-pub fn part1(aoc: &AOC) -> u32 {
+pub fn part1(aoc: &AOC) -> usize {
     let input = aoc.get_input(1).unwrap();
+    let bytes = input.as_bytes();
 
-    input.lines().fold(0, |acc, line| {
-        let digits: Vec<char> = line
-            .chars()
-            .filter_map(|c| if c.is_digit(10) { Some(c) } else { None })
-            .collect();
-        acc + format!("{}{}", digits.first().unwrap(), digits.last().unwrap())
-            .parse::<u32>()
-            .unwrap()
+    bytes.split(|b| b == &b'\n').fold(0, |acc, l| {
+        if let Some(first) = l.iter().find(|c| c.is_ascii_digit()) {
+            let last = l.iter().rev().find(|c| c.is_ascii_digit()).unwrap_or(first);
+            acc + (((first - b'0') * 10) + (last - b'0')) as usize
+        } else {
+            acc
     })
 }
